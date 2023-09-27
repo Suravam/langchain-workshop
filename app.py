b9a09398d465443dbbf4f1a699e3171799290c8a
Saravanakumar Dhanabal <saravanakumar.dhanabal@viasat.com>
2023-09-27 21:45:21 +0530

    Intial Commit LLAMA

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+from langchain.llms import OpenAI
+from bot import Bot
+#from env import OPENAI_API_KEY
+
+OPENAI_API_KEY = "APIKey"
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+  return render_template("chat.html")
+
+
+@app.post("/chat")
+def chat():
+  question = request.json["question"]
+  # llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+  # return llm.predict(question)
+  return Bot.ask(question)
